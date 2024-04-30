e7e8e3adf9dda38c8186f11758111857889e41c1
Adethapiper <adethapiper1@gmail.com>
2024-04-30 10:01:13 -0400

    exercises

---
+++ exercises/add_numbers.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function addNumbers(num1, num2) {
+    let result = num1 + num2;
+
+    console.log(num1 + " + " + num2 +" =",result); 
+}
+addNumbers(5,7);
---
+++ exercises/more_functions.js
@@ -0,0 +1,9 @@
+
+function displayMailingLabel(name, address, city, state, zip) {
+    // console.log(name, address, city, state, zip); 
+    console.log(name); 
+    console.log(address); 
+    console.log(city + ",", state, zip); 
+    console.log();
+}
+displayMailingLabel("adeyemi", "Remsen avenue", "Brooklyn", "New york", 12345);
